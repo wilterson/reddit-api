eca3948784792339326cfa63578fd80b58a51390
Wilterson Garcia <wilterson@peepi.com.br>
2021-07-31 19:21:49 -0300

    Added API Docs

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import 'reflect-metadata';
 import { Router, Request, Response } from 'express';
 
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = require('../swagger.json');
+
 import PostController from './controller/PostController'
 import UserController from './controller/UserController';
 
@@ -9,4 +13,7 @@ const routes = Router();
 routes.get('/posts', PostController.index);
 routes.get('/users', UserController.index);
 
+// Docs endpoints
+routes.use('/docs', swaggerUi.serve);
+routes.get('/docs', swaggerUi.setup(swaggerOptions));
 export default routes;
